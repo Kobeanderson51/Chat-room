5dedede15b997bf53cde363cba9b03995b5a34a4
Kobeanderson51 <79375386+Kobeanderson51@users.noreply.github.com>
2024-05-13 10:11:01 -0600

    finished server/client

--- server.js
+++ server.js
@@ -26,25 +26,42 @@ const server = net.createServer((socket) => {
                 broadcast(`${oldName} changed name to ${newName}`, socket);
             }
         } else if (message.startsWith('/kick')) {
-            const args = message.split(' ');
-            if (args.length !== 3 || args[2] !== 'kobe123') {
+            const usernameToKick = message.split(' ')[1];
+            const password = message.split(' ')[2];
+            if (!password) {
+                console.log('Invalid kick command. Format: /kick username password\n');
                 socket.write('Invalid kick command. Format: /kick username password\n');
             } else {
-                const usernameToKick = args[1];
-                const clientToKick = clients.find(client => client.clientName === usernameToKick);
-                if (clientToKick) {
-                    clientToKick.socket.write('You have been kicked from the server.\n');
-                    clientToKick.socket.end();
-                    clients.splice(clients.indexOf(clientToKick), 1);
+                if (password === "k0b3") {
+                    const clientToKick = clients.find(client => client.clientName === usernameToKick);
+                    if (clientToKick) {
+                        clientToKick.socket.write('You have been kicked from the server.\n');
+                        clientToKick.socket.end();
+                        console.log(`${usernameToKick} has been kicked from the server`)
+                    } else {
+                        socket.write(`User ${usernameToKick} not found.\n`);
+                    }
                 } else {
-                    socket.write(`User ${usernameToKick} not found.\n`);
+                    console.log('Invalid password.\n');
+                    socket.write('Invalid password.\n');
                 }
             }
-        } else if (message.startsWith('/list')) {
+        } else if (message.startsWith('/dm')) {
+        const recipient = message.split(' ')[1];
+        const dmContent = message.substring(5 + recipient.length).trim();
+        const senderName = clients.find(client => client.socket === socket).clientName;
+        const recipientClient = clients.find(client => client.clientName === recipient);
+        if (recipientClient) {
+            recipientClient.socket.write(`[DM] ${senderName}: ${dmContent}\n`);
+            socket.write(`[DM] ${senderName} to ${recipient}: ${dmContent}\n`);
+        } else {
+            socket.write(`User ${recipient} not found or offline.\n`);
+        }
+    } else if (message.startsWith('/list')) {
             const clientNames = clients.map(client => client.clientName).join(', ');
             const listMessage = `Connected users: ${clientNames}\n`;
             socket.write(listMessage);
-        } else if (message === '@exit') {
+        } else if(message === '@exit') {
             console.log(`${clientName} left the server`);
             broadcast(`${clientName} left the server`, socket);
             socket.end();
@@ -56,9 +73,13 @@ const server = net.createServer((socket) => {
     });
 
     socket.on('end', () => {
-        console.log(`${clientName} left the server`);
-        clients.splice(clients.findIndex(c => c.socket === socket), 1);
-        broadcast(`${clientName} left the server`, socket);
+        const index = clients.findIndex(client => client.socket === socket);
+        if (index !== -1) {
+            const exitingClientName = clientName || 'Anonymous';
+            clients.splice(index, 1);
+            broadcast(`${exitingClientName} left the server\n`, socket);
+            console.log(`${exitingClientName} left the server`);
+        }
     });
 });
 
